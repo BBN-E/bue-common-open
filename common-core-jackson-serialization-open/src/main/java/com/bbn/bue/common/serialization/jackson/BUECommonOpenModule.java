a37fa94844a3b85598d8fc22dff9d0d9a5dcf1f7
Ryan Gabbard <rgabbard@bbn.com>
2014-04-15 12:32:17 -0400

    Shift Symbol mix-in from internal bue-common

---
+++ common-core-jackson-serialization-open/src/main/java/com/bbn/bue/common/serialization/jackson/BUECommonOpenModule.java
@@ -0,0 +1,20 @@
+package com.bbn.bue.common.serialization.jackson;
+
+
+import com.bbn.bue.common.serialization.jackson.mixins.SymbolMixin;
+import com.bbn.bue.common.symbols.Symbol;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+public final class BUECommonOpenModule extends SimpleModule {
+    private static final long serialVersionUID = 1L;
+
+    public BUECommonOpenModule() {
+        super("BUECommonOpenModule", new Version(2,1,0,null, "com.bbn.bue", "common-core-open"));
+    }
+
+    @Override
+    public void setupModule(final SetupContext context) {
+        context.setMixInAnnotations(Symbol.class, SymbolMixin.class);
+    }
+}
