84b13be2fd081895c50677ef49ca32d14af32d42
Ryan Gabbard <rgabbard@bbn.com>
2014-08-13 15:44:26 -0400

    Scale P/R/F by 100.0 for more readability and less confusion.

--- common-core-open/src/main/java/com/bbn/bue/common/evaluation/FMeasureCounts.java
+++ common-core-open/src/main/java/com/bbn/bue/common/evaluation/FMeasureCounts.java
@@ -1,20 +1,16 @@
 package com.bbn.bue.common.evaluation;
 
+import com.google.common.annotations.Beta;
+import com.google.common.base.Objects;
+import com.google.common.collect.*;
+import com.google.common.primitives.Doubles;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import com.google.common.annotations.Beta;
-import com.google.common.base.Objects;
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Multiset;
-import com.google.common.collect.Ordering;
-import com.google.common.collect.Sets;
-import com.google.common.primitives.Doubles;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -64,7 +60,7 @@ public final class FMeasureCounts extends FMeasureInfo {
 
 	public String compactPrettyString () {
 		return String.format("TP: %.2f, FP: %.2f, FN: %.2f; P: %3.2f; R: %3.2f; F: %3.2f",
-			truePositives, falsePositives, falseNegatives, precision(), recall(), F1());
+			truePositives, falsePositives, falseNegatives, 100.0*precision(), 100.0*recall(), 100.0*F1());
 	}
 
 	public static FMeasureCounts combineToMicroFMeasure(final Iterable<FMeasureCounts> infos) {
