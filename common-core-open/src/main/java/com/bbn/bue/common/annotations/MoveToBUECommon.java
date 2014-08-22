9fc8abf17a6cc23853884a56d4c389f15836212f
Ryan Gabbard <rgabbard@bbn.com>
2014-08-22 17:19:55 -0400

    Annotation on internal code which is a candidate to move to bue-common

---
+++ common-core-open/src/main/java/com/bbn/bue/common/annotations/MoveToBUECommon.java
@@ -0,0 +1,12 @@
+package com.bbn.bue.common.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.ANNOTATION_TYPE,ElementType.FIELD,ElementType.METHOD,
+ElementType.PACKAGE, ElementType.TYPE})
+public @interface MoveToBUECommon {
+}
