d8f7d77d1951c5b479ed9c3a33b92f7075dca8cb
Ryan Gabbard <rgabbard@bbn.com>
2014-10-17 10:38:42 -0400

    Shift GZIPByteSource/Sink from closed-source common code

---
+++ common-core-open/src/main/java/com/bbn/bue/common/io/GZIPByteSink.java
@@ -0,0 +1,26 @@
+package com.bbn.bue.common.io;
+
+import com.google.common.io.ByteSink;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public final class GZIPByteSink extends ByteSink {
+	private GZIPByteSink(final ByteSink wrappedByteSink) {
+		this.wrappedByteSink = checkNotNull(wrappedByteSink);
+	}
+
+	public static ByteSink gzipCompress(final ByteSink byteSink) {
+		return new GZIPByteSink(byteSink);
+	}
+
+	@Override
+	public OutputStream openStream() throws IOException {
+		return new GZIPOutputStream(wrappedByteSink.openBufferedStream());
+	}
+
+	private final ByteSink wrappedByteSink;
+}
---
+++ common-core-open/src/main/java/com/bbn/bue/common/io/GZIPByteSource.java
@@ -0,0 +1,34 @@
+package com.bbn.bue.common.io;
+
+import com.google.common.annotations.Beta;
+import com.google.common.io.ByteSource;
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+@Beta
+public class GZIPByteSource extends ByteSource {
+	private GZIPByteSource(final ByteSource wrappedByteSource) {
+		this.wrappedByteSource = checkNotNull(wrappedByteSource);
+	}
+
+	public static ByteSource fromCompressed(final ByteSource wrappedByteSource) {
+		return new GZIPByteSource(wrappedByteSource);
+	}
+
+	public static ByteSource fromCompressed(final File f) {
+		return fromCompressed(Files.asByteSource(f));
+	}
+
+	@Override
+	public InputStream openStream() throws IOException {
+		return new GZIPInputStream(wrappedByteSource.openBufferedStream());
+	}
+
+	private final ByteSource wrappedByteSource;
+}
