0904621b79ae39edeef1b44b8aad4da9f33f3fee
Ryan Gabbard <rgabbard@bbn.com>
2015-01-27 16:54:16 -0500

    Convert to Google Java Style

--- MathUtils.java
+++ MathUtils.java
@@ -3,35 +3,36 @@ package com.bbn.bue.common.math;
 import static com.google.common.base.Preconditions.checkArgument;
 
 public final class MathUtils {
-	private MathUtils() {
-		throw new UnsupportedOperationException();
-	}
-	
-	public static int max(int[] arr) {
-		checkArgument(arr.length > 0);
-		int mx = Integer.MIN_VALUE;
-		
-		for (final int x : arr) {
-			if (x > mx) {
-				mx = x;
-			}
-		}
-		return mx;
-	}
 
-	public static int sum(int[] permutation) {
-		int ret = 0;
-		for (final int x : permutation) {
-			ret += x;
-		}
-		return ret;
-	}
-	
-	public static double xLogX(double d) {
-		if (d == 0.0) {
-			return 0.0;
-		} else {
-			return d*Math.log(d);
-		}
-	}
+  private MathUtils() {
+    throw new UnsupportedOperationException();
+  }
+
+  public static int max(int[] arr) {
+    checkArgument(arr.length > 0);
+    int mx = Integer.MIN_VALUE;
+
+    for (final int x : arr) {
+      if (x > mx) {
+        mx = x;
+      }
+    }
+    return mx;
+  }
+
+  public static int sum(int[] permutation) {
+    int ret = 0;
+    for (final int x : permutation) {
+      ret += x;
+    }
+    return ret;
+  }
+
+  public static double xLogX(double d) {
+    if (d == 0.0) {
+      return 0.0;
+    } else {
+      return d * Math.log(d);
+    }
+  }
 }
