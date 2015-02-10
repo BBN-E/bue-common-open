5f1454648253b60047ed44ba6ff97a514604d607
Ryan Gabbard <rgabbard@bbn.com>
2015-02-10 16:38:09 -0500

    Adds offset indices, which map from keys to character offsets.
    These are useful for dealing with large concatenated files, like the way Gigaword comes on the CD

---
+++ common-core-open/src/main/java/com/bbn/bue/common/io/OffsetIndex.java
@@ -0,0 +1,20 @@
+package com.bbn.bue.common.io;
+
+import com.bbn.bue.common.strings.offsets.CharOffset;
+import com.bbn.bue.common.strings.offsets.OffsetRange;
+import com.bbn.bue.common.symbols.Symbol;
+
+import com.google.common.base.Optional;
+
+import java.util.Set;
+
+/**
+ * Maps keys to offset ranges.  This is useful when, for example, many documents are concatenated
+ * together and you wish to pull on eout of the middle.
+ */
+public interface OffsetIndex {
+
+  public Optional<OffsetRange<CharOffset>> charOffsetsOf(Symbol key);
+
+  public Set<Symbol> keySet();
+}
