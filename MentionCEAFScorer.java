8e793385699750a6f4282f43d9d3d70ee9793438
Ryan Gabbard <rgabbard@bbn.com>
2015-03-03 17:25:04 -0500

    Create MentionCEAFScorer via static factory

--- MentionCEAFScorer.java
+++ MentionCEAFScorer.java
@@ -21,16 +21,23 @@ import java.util.Set;
 
 /**
  * This implements the Constrained Entity-Alignment F-Measure (CEAF) metric.
- * CEAF treats the gold and system entities as a bipartite graph (with gold vs system entities forming the two sides of the graph).  
- * Each gold entity is constrained to align with at most one system entity, and vice-versa.  
- * The best alignment is a maximum weight bipartite matching problem, which can be solved in polynomial time by the Kuhn-Munkres algorithm. 
- * 
- * For reference see: On Coreference Resolution Performance Metrics. Xiaoqiang Luo. In HLT-EMNLP 2005. Pages 25-32.  
- * 
- * In the abnove paper, Luo introduced mention-based CEAF and entity-based CEAF. 
+ * CEAF treats the gold and system entities as a bipartite graph (with gold vs system entities forming the two sides of the graph).
+ * Each gold entity is constrained to align with at most one system entity, and vice-versa.
+ * The best alignment is a maximum weight bipartite matching problem, which can be solved in polynomial time by the Kuhn-Munkres algorithm.
+ *
+ * For reference see: On Coreference Resolution Performance Metrics. Xiaoqiang Luo. In HLT-EMNLP 2005. Pages 25-32.
+ *
+ * In the above paper, Luo introduced mention-based CEAF and entity-based CEAF.
  * We implement mention-based CEAF here, i.e. using Equation (8) of the paper as the similarity measure between two entities.
  */
 public final class MentionCEAFScorer {
+
+  private MentionCEAFScorer() {
+  }
+
+  public static MentionCEAFScorer create() {
+    return new MentionCEAFScorer();
+  }
 
   public FMeasureInfo score(final Iterable<? extends Iterable<?>> predicted,
       final Iterable<? extends Iterable<?>> gold) {
