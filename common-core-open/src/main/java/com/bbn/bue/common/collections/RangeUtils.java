5330f4ff33329b456d29edef74d7333909f5bfd0
Ryan Gabbard <rgabbard@bbn.com>
2015-06-19 15:50:50 -0400

    Adds Javadoc

--- common-core-open/src/main/java/com/bbn/bue/common/collections/RangeUtils.java
+++ common-core-open/src/main/java/com/bbn/bue/common/collections/RangeUtils.java
@@ -8,6 +8,10 @@ import com.google.common.collect.Range;
  */
 public class RangeUtils {
 
+  /**
+   * Returns true iff {@code range} is bounded and closed on both sides.  {@code range}
+   * may not be null.
+   */
   public static boolean isClosed(final Range<?> range) {
     return range.hasUpperBound() && BoundType.CLOSED.equals(range.upperBoundType())
         && range.hasLowerBound() && BoundType.CLOSED.equals(range.lowerBoundType());
