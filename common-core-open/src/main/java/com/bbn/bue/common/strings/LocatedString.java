12736a216b1794e1b6e2d3ddba8c77d1b07d26e3
Jay DeYoung <jdeyoung@bbn.com>
2015-07-06 11:39:42 -0400

    new EDTOffset (deprecated) -> asEDTOffset

--- common-core-open/src/main/java/com/bbn/bue/common/strings/LocatedString.java
+++ common-core-open/src/main/java/com/bbn/bue/common/strings/LocatedString.java
@@ -370,11 +370,11 @@ public final class LocatedString {
             (edtOffset == 0 || prevChar == '\r') ? edtOffset : (edtOffset - 1);
         offsets.add(
             new OffsetEntry(startPos, pos, start, OffsetGroup.from(new ByteOffset(byteOffset - 1),
-                new CharOffset(charOffset - 1), new EDTOffset(prevEDTOffset)), justLeftXMLTag));
+                new CharOffset(charOffset - 1), EDTOffset.asEDTOffset(prevEDTOffset)), justLeftXMLTag));
         startPos = pos;
         final int startEDTOffset = (c == '<') ? edtOffset - 1 : edtOffset;
         start = OffsetGroup.from(new ByteOffset(byteOffset), new CharOffset(charOffset),
-            new EDTOffset(startEDTOffset));
+            EDTOffset.asEDTOffset(startEDTOffset));
       }
 
       ++charOffset;
@@ -399,7 +399,7 @@ public final class LocatedString {
       final int prevEDTOffset = Math.max(start.edtOffset().value(), edtOffset - 1);
       offsets.add(new OffsetEntry(startPos, pos, start,
           OffsetGroup.from(new ByteOffset(byteOffset - 1), new CharOffset(charOffset - 1),
-              new EDTOffset(prevEDTOffset)), inTag > 0 || justLeftXMLTag));
+              EDTOffset.asEDTOffset(prevEDTOffset)), inTag > 0 || justLeftXMLTag));
     }
     return offsets.build();
   }
