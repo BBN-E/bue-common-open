c89574e155d83e655e10c9bc6b0ba8b8dae345ed
Ryan Gabbard <rgabbard@bbn.com>
2015-07-21 11:49:55 -0400

    Add Manufacture.Artifact ERE types

--- ERETypes.java
+++ ERETypes.java
@@ -8,6 +8,7 @@ public final class ERETypes {
   public static final Symbol MOVEMENT_TRANSPORT_ARTIFACT = Symbol.from(
       "Movement.Transport-Artifact");
   public static final Symbol MOVEMENT_TRANSPORT_PERSON = Symbol.from("Movement.Transport-Person");
+  public static final Symbol MANUFACTURE_ARTIFACT = Symbol.from("Manufacture.Artifact");
 
   private ERETypes() {
     throw new UnsupportedOperationException();
