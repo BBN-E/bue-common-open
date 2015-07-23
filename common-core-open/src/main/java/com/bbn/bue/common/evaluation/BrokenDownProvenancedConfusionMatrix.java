d32a866015c2651bc0b8716d89f7682d8675fbbb
Jay DeYoung <jdeyoung@bbn.com>
2015-07-23 13:57:52 -0400

    extract analytic packages to bue-common-open from the kbp-events2014-scorer

---
+++ common-core-open/src/main/java/com/bbn/bue/common/evaluation/BrokenDownProvenancedConfusionMatrix.java
@@ -0,0 +1,34 @@
+package com.bbn.bue.common.evaluation;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+public final class BrokenDownProvenancedConfusionMatrix<SignatureType, ProvenanceType> {
+
+  private ImmutableMap<SignatureType, ProvenancedConfusionMatrix<ProvenanceType>> data;
+
+  private BrokenDownProvenancedConfusionMatrix(
+      Map<SignatureType, ProvenancedConfusionMatrix<ProvenanceType>> data) {
+    this.data = ImmutableMap.copyOf(data);
+  }
+
+  public static <SignatureType, ProvenanceType> BrokenDownProvenancedConfusionMatrix fromMap(
+      Map<SignatureType, ProvenancedConfusionMatrix<ProvenanceType>> data) {
+    return new BrokenDownProvenancedConfusionMatrix(data);
+  }
+
+
+  public Map<SignatureType, ProvenancedConfusionMatrix<ProvenanceType>> asMap() {
+    return data;
+  }
+
+  public BrokenDownSummaryConfusionMatrix toSummary() {
+    final ImmutableMap.Builder<SignatureType, SummaryConfusionMatrix> ret = ImmutableMap.builder();
+    for (final Map.Entry<SignatureType, ProvenancedConfusionMatrix<ProvenanceType>> entry : data
+        .entrySet()) {
+      ret.put(entry.getKey(), entry.getValue().buildSummaryMatrix());
+    }
+    return BrokenDownSummaryConfusionMatrix.fromMap(ret.build());
+  }
+}
