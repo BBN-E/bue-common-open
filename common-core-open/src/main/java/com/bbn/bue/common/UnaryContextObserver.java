a3256c00dcc9d5d284076f00a72fdc271b0758ad
Ryan Gabbard <rgabbard@bbn.com>
2015-10-09 16:51:44 -0400

    Interfaces for observers of single or paired items in some context.
    This is a pattern which occurs frequently in NLP work.  For example, in a scorer, we may observer paired event mentions in the context of some document.

--- common-core-open/src/main/java/com/bbn/bue/common/UnaryContextObserver.java
+++ common-core-open/src/main/java/com/bbn/bue/common/UnaryContextObserver.java
@@ -1,5 +1,7 @@
 package com.bbn.bue.common;
 
+import com.google.common.annotations.Beta;
+
 /**
  * An object which can observe some item (of type {@code T}) in some context (of type {@code Ctx}).
  * {@link #finish()}  is expected to be called when there are no more items to observe. What happens
@@ -8,6 +10,7 @@ package com.bbn.bue.common;
  * @param <CtxT> Covariant
  * @param <T>    Covariant
  */
+@Beta
 public interface UnaryContextObserver<CtxT, T> extends Finishable {
 
   void observe(CtxT id, T item);
