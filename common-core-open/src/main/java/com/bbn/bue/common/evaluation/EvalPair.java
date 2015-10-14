e0d9fe2f3b736acbf5bd09780e722e51f1c32ce3
Ryan Gabbard <rgabbard@bbn.com>
2015-10-14 16:12:57 -0400

    Adds class to represent a pair of things being compared to each other, where one is in some sense "the correct one" (the key).

--- common-core-open/src/main/java/com/bbn/bue/common/evaluation/EvalPair.java
+++ common-core-open/src/main/java/com/bbn/bue/common/evaluation/EvalPair.java
@@ -4,6 +4,13 @@ import com.google.common.base.Function;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * A pair of things being compared to each other, where one is in some sense "the correct one" (the
+ * key).
+ *
+ * @param <KeyT>  The type of the "correct" item
+ * @param <TestT> The type of the other item
+ */
 public final class EvalPair<KeyT, TestT> {
 
   private final KeyT keyItem;
@@ -14,19 +21,32 @@ public final class EvalPair<KeyT, TestT> {
     this.testItem = checkNotNull(testItem);
   }
 
+  /**
+   * The correct item.
+   */
   public KeyT key() {
     return keyItem;
   }
 
+  /**
+   * The other item
+   */
   public TestT test() {
     return testItem;
   }
 
+  /**
+   * Create an {@code EvalPair}. The "correct" (key) item comes first.
+   */
   public static <KeyT, TestT> EvalPair<KeyT, TestT> of(
       KeyT key, TestT test) {
     return new EvalPair<KeyT, TestT>(key, test);
   }
 
+  /**
+   * Turns a Guava {@link Function} into another {@code Function} which operates over both sides of
+   * an {@link EvalPair}.
+   */
   public static <F, T, KeyT extends F, TestT extends F>
   Function<EvalPair<? extends KeyT, ? extends TestT>, EvalPair<T, T>> functionOnBoth(
       final Function<F, T> func) {
@@ -34,6 +54,11 @@ public final class EvalPair<KeyT, TestT> {
       @Override
       public EvalPair<T, T> apply(final EvalPair<? extends KeyT, ? extends TestT> input) {
         return EvalPair.of(func.apply(input.key()), func.apply(input.test()));
+      }
+
+      @Override
+      public String toString() {
+        return "EvalPair.functionOnBoth(" + func + ")";
       }
     };
   }
