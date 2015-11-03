41819c44a50e2e0044a5f26e9122e5923f00f7ce
Constantine Lignos <clignos@bbn.com>
2015-11-03 17:35:55 -0500

    Make cross validation batches distribute extra items evenly.

--- common-core-open/src/main/java/com/bbn/bue/common/MakeCrossValidationBatches.java
+++ common-core-open/src/main/java/com/bbn/bue/common/MakeCrossValidationBatches.java
@@ -1,13 +1,10 @@
 package com.bbn.bue.common;
 
-import com.bbn.bue.common.collections.ShufflingIterable;
 import com.bbn.bue.common.files.FileUtils;
 import com.bbn.bue.common.parameters.Parameters;
-
 import com.google.common.base.Charsets;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
 import com.google.common.io.Files;
@@ -16,13 +13,16 @@ import com.google.common.math.IntMath;
 import java.io.File;
 import java.io.IOException;
 import java.math.RoundingMode;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
 /**
  * Given a list of files and a number of splits, creates training/test file lists for
- * cross-validation.
+ * cross-validation. When the files cannot be evenly divided across all splits, extra files are
+ * distributed as evenly as possible, starting with the first folds. For example, dividing 11 items
+ * into three folds will result in folds of size (4, 4, 3).
  */
 public final class MakeCrossValidationBatches {
 
@@ -41,7 +41,15 @@ public final class MakeCrossValidationBatches {
     }
   }
 
+  private static void errorExit(final String msg) {
+    System.err.println(msg);
+    System.exit(1);
+  }
+
   private static void trueMain(String[] argv) throws IOException {
+    if (argv.length != 1) {
+      errorExit("Usage: MakeCrossValidationBatches params");
+    }
     final Parameters parameters = Parameters.loadSerifStyle(new File(argv[0]));
     final File fileList = parameters.getExistingFile("com.bbn.bue.common.crossValidation.fileList");
     final File outputDirectory = parameters.getExistingDirectory(
@@ -51,12 +59,55 @@ public final class MakeCrossValidationBatches {
         "com.bbn.bue.common.crossValidation.numBatches");
     final int randomSeed = parameters.getInteger("com.bbn.bue.common.crossValidation.randomSeed");
 
-    final ImmutableList<File> inputFiles = FileUtils.loadFileList(fileList);
+    // Load the list of files
+    final List<File> inputFiles = Lists.newArrayList(FileUtils.loadFileList(fileList));
+
+    // Check numbatches
+    if (numBatches < 2) {
+      errorExit("Bad numBatches value: Need two or more batches to divide data into");
+    } else if (numBatches > inputFiles.size()) {
+      errorExit("Bad numBatches value: Cannot create more batches than there are input files");
+    }
+
+    // Shuffle files
+    Collections.shuffle(inputFiles, new Random(randomSeed));
+    // Divide into folds, with the remainder falling into the last fold
+    List<List<File>> folds =
+        Lists.partition(inputFiles, IntMath.divide(inputFiles.size(), numBatches, RoundingMode.DOWN));
+    // If there are more folds than desired, distribute elements of the last fold to the other folds
+    if (folds.size() > numBatches) {
+      // First, make the outer list modifiable
+      folds = Lists.newLinkedList(folds);
+      // Remove the extra fold
+      final List<File> extras = folds.remove(folds.size() - 1);
+
+      // Make the folds modifiable
+      for (int i = 0; i < folds.size(); i++) {
+        folds.set(i, Lists.newLinkedList(folds.get(i)));
+      }
+
+      // Distribute the extra elements
+      for (int extraIdx = 0, foldIdx = 0; extraIdx < extras.size(); extraIdx++, foldIdx = (foldIdx + 1) % folds.size()) {
+        folds.get(foldIdx).add(extras.get(extraIdx));
+      }
+    }
+
+    // Sanity checks
+    // Correct number of folds
+    if (folds.size() != numBatches) {
+      throw new RuntimeException("Incorrect number of batches created");
+    }
+    // No elements were lost
+    int totalFiles = 0;
+    for(List<File> fold : folds) {
+      totalFiles += fold.size();
+    }
+    if (totalFiles != inputFiles.size()) {
+      throw new RuntimeException("Input files size and sum of folds do not match");
+    }
 
     int batchNum = 0;
-    for (final List<File> testFilesForBatch : Iterables
-        .partition(ShufflingIterable.from(inputFiles, new Random(randomSeed)),
-            IntMath.divide(inputFiles.size(), numBatches, RoundingMode.UP))) {
+    for (final List<File> testFilesForBatch : folds) {
       final Set<File> trainingFilesForBatch =
           Sets.difference(ImmutableSet.copyOf(inputFiles), ImmutableSet.copyOf(testFilesForBatch))
               .immutableCopy();
