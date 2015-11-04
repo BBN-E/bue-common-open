8302412861d76eea14972749695b741bdc6c0066
Constantine Lignos <clignos@bbn.com>
2015-11-04 18:28:00 -0500

    Extend crossvalidation to use docIdToFile mappings.

--- common-core-open/src/main/java/com/bbn/bue/common/MakeCrossValidationBatches.java
+++ common-core-open/src/main/java/com/bbn/bue/common/MakeCrossValidationBatches.java
@@ -3,18 +3,24 @@ package com.bbn.bue.common;
 import com.bbn.bue.common.collections.CollectionUtils;
 import com.bbn.bue.common.files.FileUtils;
 import com.bbn.bue.common.parameters.Parameters;
+import com.bbn.bue.common.symbols.Symbol;
 import com.google.common.base.Charsets;
+import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
 import com.google.common.io.Files;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
@@ -58,59 +64,103 @@ public final class MakeCrossValidationBatches {
     final int numBatches = parameters.getPositiveInteger(
         "com.bbn.bue.common.crossValidation.numBatches");
     final int randomSeed = parameters.getInteger("com.bbn.bue.common.crossValidation.randomSeed");
+    final boolean useDocIdMap = parameters.getBoolean("com.bbn.bue.common.crossValidation.useDocIdMap");
 
-    // Load the list of files. We use ArrayList as we will sort in place shortly.
-    final List<File> inputFiles = Lists.newArrayList(FileUtils.loadFileList(fileList));
-
-    // Check for duplicates
-    if (Sets.newHashSet(inputFiles).size() != inputFiles.size()) {
-      errorExit("Input file list contains duplicate entries");
-    }
-
-    // Check numbatches
     if (numBatches < 2) {
       errorExit("Bad numBatches value: Need two or more batches to divide data into");
-    } else if (numBatches > inputFiles.size()) {
-      errorExit("Bad numBatches value: Cannot create more batches than there are input files");
     }
 
-    // Shuffle files
-    Collections.shuffle(inputFiles, new Random(randomSeed));
-    // Divide into folds
-    final ImmutableList<ImmutableList<File>> folds =
-        CollectionUtils.partition(inputFiles, numBatches);
+    if (useDocIdMap) {
+      // For the document ID map
+      final ImmutableMap<Symbol, File> docIdMap =
+          FileUtils.loadSymbolToFileMap(Files.asCharSource(fileList, Charsets.UTF_8));
 
-    // Sanity checks
-    // Correct number of folds
-    if (folds.size() != numBatches) {
-      throw new RuntimeException("Incorrect number of batches created");
-    }
-    // No elements were lost
-    int totalFiles = 0;
-    for(List<File> fold : folds) {
-      totalFiles += fold.size();
-    }
-    if (totalFiles != inputFiles.size()) {
-      throw new RuntimeException("Input files size and sum of folds do not match");
-    }
+      // Get the list of docids and shuffle them
+      final ArrayList<Symbol> docIds = Lists.newArrayList(docIdMap.keySet());
+      if (numBatches > docIds.size()) {
+        errorExit("Bad numBatches value: Cannot create more batches than there are input files");
+      }
+      Collections.shuffle(docIds, new Random(randomSeed));
 
-    int batchNum = 0;
-    for (final List<File> testFilesForBatch : folds) {
-      final Set<File> trainingFilesForBatch =
-          Sets.difference(ImmutableSet.copyOf(inputFiles), ImmutableSet.copyOf(testFilesForBatch))
-              .immutableCopy();
+      // Divide into folds
+      final ImmutableList<ImmutableList<Symbol>> folds =
+          CollectionUtils.partition(docIds, numBatches);
 
-      final File trainingOutputFile = new File(outputDirectory, outputName + "." +
-          StringUtils.padWithMax(batchNum, numBatches - 1) + ".training.list");
-      FileUtils.writeFileList(Ordering.natural().sortedCopy(trainingFilesForBatch),
-          Files.asCharSink(trainingOutputFile,
-              Charsets.UTF_8));
-      final File testOutputFile = new File(outputDirectory, outputName + "."+
-          StringUtils.padWithMax(batchNum, numBatches - 1) + ".test.list");
-      FileUtils.writeFileList(Ordering.natural().sortedCopy(testFilesForBatch),
-          Files.asCharSink(testOutputFile,
-              Charsets.UTF_8));
-      ++batchNum;
+      int batchNum = 0;
+      int totalTest = 0;
+      for (final List<Symbol> docIdsForBatch : folds) {
+        final Set<Symbol> testDocIds = ImmutableSet.copyOf(docIdsForBatch);
+        final Set<Symbol> trainDocIds =
+            Sets.difference(ImmutableSet.copyOf(docIds), testDocIds).immutableCopy();
+
+        final Map<Symbol, File> trainDocIdMap =
+            Maps.filterKeys(docIdMap, new Predicate<Symbol>() {
+              @Override
+              public boolean apply(Symbol input) {
+                return trainDocIds.contains(input);
+              }
+            });
+        final Map<Symbol, File> testDocIdMap =
+            Maps.filterKeys(docIdMap, new Predicate<Symbol>() {
+              @Override
+              public boolean apply(Symbol input) {
+                return testDocIds.contains(input);
+              }
+            });
+
+        final File trainingOutputFile = new File(outputDirectory, outputName + "." +
+            StringUtils.padWithMax(batchNum, numBatches - 1) + ".training.docIDToFileMap");
+        FileUtils.writeSymbolToFileMap(trainDocIdMap, Files.asCharSink(trainingOutputFile,
+            Charsets.UTF_8));
+        final File testOutputFile = new File(outputDirectory, outputName + "." +
+            StringUtils.padWithMax(batchNum, numBatches - 1) + ".test.docIDToFileMap");
+        FileUtils.writeSymbolToFileMap(testDocIdMap, Files.asCharSink(testOutputFile,
+            Charsets.UTF_8));
+        ++batchNum;
+        totalTest += testDocIdMap.size();
+      }
+      if(totalTest != docIdMap.size()) {
+        errorExit("Test files created are not the same length as the input");
+      }
+    } else {
+
+      // Load the list of files and shuffle.
+      final List<File> inputFiles = Lists.newArrayList(FileUtils.loadFileList(fileList));
+      if (Sets.newHashSet(inputFiles).size() != inputFiles.size()) {
+        errorExit("Input file list contains duplicate entries");
+      }
+      else if (numBatches > inputFiles.size()) {
+        errorExit("Bad numBatches value: Cannot create more batches than there are input files");
+      }
+      Collections.shuffle(inputFiles, new Random(randomSeed));
+
+      // Divide into folds
+      final ImmutableList<ImmutableList<File>> folds =
+          CollectionUtils.partition(inputFiles, numBatches);
+
+      int batchNum = 0;
+      int totalTest = 0;
+      for (final List<File> testFilesForBatch : folds) {
+        final Set<File> trainingFilesForBatch =
+            Sets.difference(ImmutableSet.copyOf(inputFiles), ImmutableSet.copyOf(testFilesForBatch))
+                .immutableCopy();
+
+        final File trainingOutputFile = new File(outputDirectory, outputName + "." +
+            StringUtils.padWithMax(batchNum, numBatches - 1) + ".training.list");
+        FileUtils.writeFileList(Ordering.natural().sortedCopy(trainingFilesForBatch),
+            Files.asCharSink(trainingOutputFile,
+                Charsets.UTF_8));
+        final File testOutputFile = new File(outputDirectory, outputName + "." +
+            StringUtils.padWithMax(batchNum, numBatches - 1) + ".test.list");
+        FileUtils.writeFileList(Ordering.natural().sortedCopy(testFilesForBatch),
+            Files.asCharSink(testOutputFile,
+                Charsets.UTF_8));
+        ++batchNum;
+        totalTest += testFilesForBatch.size();
+      }
+      if(totalTest != inputFiles.size()) {
+        errorExit("Test files created are not the same length as the input");
+      }
     }
   }
 }
