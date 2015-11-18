0bb3c3c94385d711233417c40fcca379086bb514
Ryan Gabbard <rgabbard@bbn.com>
2015-11-18 11:49:01 -0500

    Deprecate public constructor of NullByteSink

--- common-core-open/src/main/java/com/bbn/bue/common/io/NullByteSink.java
+++ common-core-open/src/main/java/com/bbn/bue/common/io/NullByteSink.java
@@ -6,7 +6,23 @@ import com.google.common.io.ByteStreams;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * A {@link ByteSink} which throws away everything written to it.
+ */
 public final class NullByteSink extends ByteSink {
+
+  /**
+   * Prefer {@link #create()}
+   */
+  @Deprecated
+  public NullByteSink() {
+
+  }
+
+  @SuppressWarnings("deprecated")
+  public static ByteSink create() {
+    return new NullByteSink();
+  }
 
   @Override
   public OutputStream openStream() throws IOException {
