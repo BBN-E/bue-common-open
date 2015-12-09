e8519e95aefa72fb393f3ed0c502fa6503d5e779
Ryan Gabbard <rgabbard@bbn.com>
2015-12-09 17:15:03 -0500

    Remove giant commented-out method

--- nlp-core-open/src/main/java/com/bbn/nlp/corpora/ere/EREDocument.java
+++ nlp-core-open/src/main/java/com/bbn/nlp/corpora/ere/EREDocument.java
@@ -3,7 +3,6 @@ package com.bbn.nlp.corpora.ere;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
-import java.io.IOException;
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -102,110 +101,4 @@ public final class EREDocument {
     return text.replaceAll("\\n+", "").replace("&", " &amp; ");
   }
 
-  public void writeToAPFfile(String strFileApf, String strFileSgm) throws IOException {
-    /*
-    StringBuffer s = new StringBuffer("");
-
-    PrintWriter pwApf = new PrintWriter(new OutputStreamWriter(new FileOutputStream(strFileApf), "UTF-8"));
-
-    s.append("<document DOCID=\"" + docid + "\">\n");
-
-    // entity
-    for(final APFEntity entity : entities) {
-      s.append("<entity ID=\"" + entity.getID() + "\" TYPE=\"" + entity.getType() +
-          "\" SUBTYPE=\"" + entity.getSubtype() + "\" CLASS=\"" + entity.getCls() + "\">\n");
-
-      for(final APFEntityMention entityMention : entity.getMentions()) {
-        s.append(Strings.repeat(" ", 2) + "<entity_mention ID=\"" + entityMention.getID() +
-            "\" TYPE=\"" + entityMention.getType() + "\" LDCTYPE=\"" + entityMention.getLdcType() +
-            "\" LDCATR=\"" + entityMention.getLdcAtr() + "\">\n");
-
-        s.append(Strings.repeat(" ", 4) + "<extent>\n");
-
-        s.append(Strings.repeat(" ", 6) + "<charseq START=\"" + entityMention.getExtent().getStart() + "\" END=\""
-            + entityMention.getExtent().getEnd() + "\">" +
-            formatText(entityMention.getExtent().getText()) + "</charseq>\n";
-
-        s.append(Strings.repeat(" ", 4) + "</extent>\n");
-
-        s.append(Strings.repeat(" ", 4) + "<head>\n");
-
-        s.append(Strings.repeat(" ",  6) + "<charseq START=\"" + entityMention.getHead().getStart() + "\" END=\""
-            + entityMention.getHead().getEnd() + "\">" +
-            formatText(entityMention.getHead().getText()) + "</charseq>\n";
-
-        s.append(Strings.repeat(" ", 4) + "</head>\n");
-
-        s.append(Strings.repeat(" ", 2) + "</entity_mention>\n");
-      }
-
-      s.append("</entity>\n");
-    }
-
-    for (APFValue value : values) {
-      APFValueMention valueMention = value.getValueMention();
-      strAceDoc +=
-          "<value ID=\"" + value.getID() + "\" TYPE=\"" + value.getType() + "\" SUBTYPE=\"" + value
-              .getSubtype() + "\">\n";
-      strAceDoc += "  <value_mention ID=\"" + valueMention.getID() + "\">\n";
-      strAceDoc += "    <extent>\n";
-      strAceDoc += "      <charseq START=\"" + valueMention.getExtent().getStart() + "\" END=\""
-          + valueMention.getExtent().getEnd() + "\">" +
-          formatText(valueMention.getExtent().getText()) + "</charseq>\n";
-      strAceDoc += "    </extent>\n";
-      strAceDoc += "  </value_mention>\n";
-      strAceDoc += "</value>\n";
-    }
-
-    for (APFRelation relation : relations) {
-      strAceDoc +=
-          "<relation ID=\"" + relation.getID() + "\" TYPE=\"" + relation.getType() + "\" SUBTYPE=\""
-              + relation.getSubtype() +
-              "\" TENSE=\"" + relation.getTense() + "\" MODALITY=\"" + relation.getModality()
-              + "\">\n";
-
-      for (String role : new TreeSet<String>((relation.getRoles()))) {
-        APFMentionIterable<? extends APFSpanning> arg = relation.getArgument(role).get();
-        strAceDoc += "  <relation_argument REFID=\"" + arg.getID() + "\" ROLE=\"" + role + "\"/>\n";
-      }
-
-      for (APFRelationMention relMention : relation.getRelationMentions()) {
-        strAceDoc +=
-            "  <relation_mention ID=\"" + relMention.getID() + "\" LEXICALCONDITION=\"" + relMention
-                .getLexicalCondition() + "\">\n";
-        if (relMention.getExtent().isPresent()) {
-          strAceDoc += "    <extent>\n";
-          strAceDoc +=
-              "      <charseq START=\"" + relMention.getExtent().get().getStart() + "\" END=\""
-                  + relMention.getExtent().get().getEnd() + "\">" +
-                  formatText(relMention.getExtent().get().getText()) + "</charseq>\n";
-          strAceDoc += "    </extent>\n";
-        }
-
-        for (String role : new TreeSet<String>(relMention.getRoles())) {
-          APFRelationMentionArgument relMenArg = relMention.getArgument(role).get();
-          strAceDoc += "    <relation_mention_argument REFID=\"" + relMenArg.getMention().getID()
-              + "\" ROLE=\"" + role + "\">\n";
-          if (relMenArg.getExtent().isPresent()) {
-            strAceDoc += "      <extent>\n";
-            strAceDoc +=
-                "        <charseq START=\"" + relMenArg.getExtent().get().getStart() + "\" END=\""
-                    + relMenArg.getExtent().get().getEnd() + "\">" +
-                    formatText(relMenArg.getExtent().get().getText()) + "</charseq>\n";
-            strAceDoc += "      </extent>\n";
-          }
-          strAceDoc += "    </relation_mention_argument>\n";
-        }
-
-        strAceDoc += "  </relation_mention>\n";
-      }
-      strAceDoc += "</relation>\n";
-    }
-
-    strAceDoc += "</document>\n" + "</source_file>\n";
-
-    pwApf.print(strAceDoc);
-    pwApf.close();
-    */
-  }
 }
