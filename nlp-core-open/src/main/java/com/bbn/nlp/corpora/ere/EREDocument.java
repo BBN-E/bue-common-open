b79887c62a08fc1ebb71ace315933b6de9765284
Ryan Gabbard <rgabbard@bbn.com>
2015-12-09 17:11:50 -0500

    Clarify all list fields on ERE document are immutable

--- nlp-core-open/src/main/java/com/bbn/nlp/corpora/ere/EREDocument.java
+++ nlp-core-open/src/main/java/com/bbn/nlp/corpora/ere/EREDocument.java
@@ -3,21 +3,21 @@ package com.bbn.nlp.corpora.ere;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import java.io.IOException;
 import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class EREDocument {
   private final String docid;
   private final String sourceType;
-  private final List<EREEntity> entities;
-  private final List<EREFiller> fillers;
-  private final List<ERERelation> relations;
-  private final List<EREEvent> events;
+  private final ImmutableList<EREEntity> entities;
+  private final ImmutableList<EREFiller> fillers;
+  private final ImmutableList<ERERelation> relations;
+  private final ImmutableList<EREEvent> events;
 
-  private EREDocument(final String docid, final String sourceType, 
-      final List<EREEntity> entities, final List<EREFiller> fillers,  
+  private EREDocument(final String docid, final String sourceType,
+      final List<EREEntity> entities, final List<EREFiller> fillers,
       final List<ERERelation> relations, final List<EREEvent> events) {
     this.docid = checkNotNull(docid);
     this.sourceType = checkNotNull(sourceType);
@@ -34,7 +34,7 @@ public final class EREDocument {
   public String getSourceType() {
     return sourceType;
   }
-  
+
   public List<EREEntity> getEntities() {
     return entities;
   }
@@ -42,7 +42,7 @@ public final class EREDocument {
   public List<EREFiller> getFillers() {
     return fillers;
   }
-  
+
   public List<ERERelation> getRelations() {
     return relations;
   }
@@ -50,11 +50,11 @@ public final class EREDocument {
   public List<EREEvent> getEvents() {
     return events;
   }
-  
+
   public static Builder builder(final String docid, final String sourceType) {
     return new Builder(docid, sourceType);
   }
-  
+
   public static class Builder {
     private final String docid;
     private final String sourceType;
@@ -62,7 +62,7 @@ public final class EREDocument {
     private final List<EREFiller> fillers;
     private final List<ERERelation> relations;
     private final List<EREEvent> events;
-    
+
     public Builder(final String docid, final String sourceType) {
       this.docid = checkNotNull(docid);
       this.sourceType = checkNotNull(sourceType);
@@ -85,7 +85,7 @@ public final class EREDocument {
       this.fillers.add(v);
       return this;
     }
-    
+
     public Builder withRelation(ERERelation r) {
       this.relations.add(r);
       return this;
@@ -95,7 +95,7 @@ public final class EREDocument {
       this.events.add(e);
       return this;
     }
-    
+
   }
 
   public static String formatText(String text) {
@@ -105,35 +105,35 @@ public final class EREDocument {
   public void writeToAPFfile(String strFileApf, String strFileSgm) throws IOException {
     /*
     StringBuffer s = new StringBuffer("");
-    
+
     PrintWriter pwApf = new PrintWriter(new OutputStreamWriter(new FileOutputStream(strFileApf), "UTF-8"));
-        
+
     s.append("<document DOCID=\"" + docid + "\">\n");
 
     // entity
     for(final APFEntity entity : entities) {
-      s.append("<entity ID=\"" + entity.getID() + "\" TYPE=\"" + entity.getType() + 
+      s.append("<entity ID=\"" + entity.getID() + "\" TYPE=\"" + entity.getType() +
           "\" SUBTYPE=\"" + entity.getSubtype() + "\" CLASS=\"" + entity.getCls() + "\">\n");
 
       for(final APFEntityMention entityMention : entity.getMentions()) {
-        s.append(Strings.repeat(" ", 2) + "<entity_mention ID=\"" + entityMention.getID() + 
-            "\" TYPE=\"" + entityMention.getType() + "\" LDCTYPE=\"" + entityMention.getLdcType() + 
+        s.append(Strings.repeat(" ", 2) + "<entity_mention ID=\"" + entityMention.getID() +
+            "\" TYPE=\"" + entityMention.getType() + "\" LDCTYPE=\"" + entityMention.getLdcType() +
             "\" LDCATR=\"" + entityMention.getLdcAtr() + "\">\n");
 
         s.append(Strings.repeat(" ", 4) + "<extent>\n");
-        
+
         s.append(Strings.repeat(" ", 6) + "<charseq START=\"" + entityMention.getExtent().getStart() + "\" END=\""
             + entityMention.getExtent().getEnd() + "\">" +
             formatText(entityMention.getExtent().getText()) + "</charseq>\n";
-        
+
         s.append(Strings.repeat(" ", 4) + "</extent>\n");
 
         s.append(Strings.repeat(" ", 4) + "<head>\n");
-        
+
         s.append(Strings.repeat(" ",  6) + "<charseq START=\"" + entityMention.getHead().getStart() + "\" END=\""
             + entityMention.getHead().getEnd() + "\">" +
             formatText(entityMention.getHead().getText()) + "</charseq>\n";
-        
+
         s.append(Strings.repeat(" ", 4) + "</head>\n");
 
         s.append(Strings.repeat(" ", 2) + "</entity_mention>\n");
