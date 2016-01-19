c706700c9e104a8a0c6812325f4197b675d94abf
Ryan Gabbard <rgabbard@bbn.com>
2016-01-19 13:13:51 -0500

    Show line number in error message

--- common-core-open/src/main/java/com/bbn/bue/common/files/FileUtils.java
+++ common-core-open/src/main/java/com/bbn/bue/common/files/FileUtils.java
@@ -271,7 +271,7 @@ public final class FileUtils {
       }
 
       if (!good || parts.hasNext()) {
-        throw new RuntimeException(String.format("Corrupt line: %s", line));
+        throw new RuntimeException(String.format("Corrupt line #%d: %s", lineNo, line));
       }
 
       try {
