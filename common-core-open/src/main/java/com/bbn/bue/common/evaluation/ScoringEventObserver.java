a857ac09753e4a028a0fb8646a13bf228b6ad92e
Constantine Lignos <clignos@bbn.com>
2016-02-09 14:59:44 -0500

    Add missing negation to documentation.

--- common-core-open/src/main/java/com/bbn/bue/common/evaluation/ScoringEventObserver.java
+++ common-core-open/src/main/java/com/bbn/bue/common/evaluation/ScoringEventObserver.java
@@ -11,8 +11,8 @@ import java.io.IOException;
  * are observed after finishing is not defined by the interface contract.
  *
  * Implementations may silently take no action for some types of observations. For example, an F-score related observer
- * would take no action upon observation of a true negative. However, an implementation should raise an exception simply
- * because it is asked to observe an event for which it will take no action.
+ * would take no action upon observation of a true negative. However, an implementation should not raise an exception
+ * simply because it is asked to observe an event for which it will take no action.
  *
  * @author Constantine Lignos
  */
