e37ba2050350726f610c75cf7549ffa7c6d3a594
Ryan Gabbard <rgabbard@bbn.com>
2016-02-10 12:36:59 -0500

    Adds ImmutableMap builders which relax various constraints
    Closes #35

--- common-core-open/src/test/java/com/bbn/bue/common/collections/MapUtilsTest.java
+++ common-core-open/src/test/java/com/bbn/bue/common/collections/MapUtilsTest.java
@@ -1,5 +1,6 @@
 package com.bbn.bue.common.collections;
 
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Ordering;
@@ -8,6 +9,9 @@ import org.junit.Test;
 
 import java.util.Map;
 
+import javax.annotation.Nullable;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 import static org.junit.Assert.assertEquals;
 
 public final class MapUtilsTest {
@@ -33,5 +37,53 @@ public final class MapUtilsTest {
 
     assertEquals(reversedReference, MapUtils.copyWithKeysSortedBy(input,
         Ordering.natural().reverse()));
+  }
+
+  @Test
+  public void testAllowSameEntry() {
+    final LaxImmutableMapBuilder<String, String> ret =
+        MapUtils.immutableMapBuilderAllowingSameEntryTwice();
+    ret.put("hello", "world");
+    ret.put("hello", "world");
+    ret.build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAllowSameEntryFail() {
+    final LaxImmutableMapBuilder<String, String> ret =
+        MapUtils.immutableMapBuilderAllowingSameEntryTwice();
+    ret.put("hello", "world");
+    ret.put("hello", "earth");
+    ret.build();
+  }
+
+  @Test
+  public void testKeepFirst() {
+    final LaxImmutableMapBuilder<String, String> ret =
+        MapUtils.immutableMapBuilderIgnoringDuplicates();
+    ret.put("hello", "world");
+    ret.put("hello", "earth");
+    final ImmutableMap<String, String> map = ret.build();
+    assertEquals(1, map.size());
+    assertEquals("world", map.get("hello"));
+  }
+
+  private static final Ordering<String> BY_REVERSE_STRING =
+      Ordering.natural().onResultOf(new Function<String, String>() {
+        @Override
+        public String apply(@Nullable final String s) {
+          return new StringBuilder(checkNotNull(s)).reverse().toString();
+        }
+      });
+
+  @Test
+  public void testBestByComparator() {
+    final LaxImmutableMapBuilder<String, String> ret =
+        MapUtils.immutableMapBuilderResolvingDuplicatesBy(BY_REVERSE_STRING);
+    ret.put("hello", "world");
+    ret.put("hello", "earth");
+    final ImmutableMap<String, String> map = ret.build();
+    assertEquals(1, map.size());
+    assertEquals("earth", map.get("hello"));
   }
 }
