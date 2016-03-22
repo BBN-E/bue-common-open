7d2802e3e6701b923bdb0e2c316ea014c9966648
Constantine Lignos <clignos@bbn.com>
2016-03-22 17:28:35 -0400

    Add ImmutableMultimapProxy and tests
    This allows Jackson serialization for ImmutableMultimaps and adds test
    coverage of ImmutableMapProxy serialization as well. Closes #45.

---
+++ common-core-jackson-serialization-open/src/main/java/com/bbn/bue/common/serialization/jackson/ImmutableMultimapProxy.java
@@ -0,0 +1,61 @@
+package com.bbn.bue.common.serialization.jackson;
+
+import com.bbn.bue.common.collections.MapUtils;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Works around a Jackson issue in serializing multimaps in a similar fashion to {@link ImmutableMapProxy}.
+ * To use this:
+ * <ol>
+ *   <li>Don't make the multimap itself a {@code JsonProperty}</li>
+ *   <li>Provide a {@code JsonProperty}-annotated package-private accessor which returns
+ *   {@code ImmutableMultimapProxy.forMultimap(myMap)}</li>
+ *   <li>In your {@code JsonCreator} method, take an {@code ImmutableMultimapProxy} as an argument and call
+ *   {@link #toImmutableMultimap()}</li>
+ * </ol>
+ */
+public class ImmutableMultimapProxy<K, V>  {
+
+  @JsonProperty("keys")
+  private final List<K> keys;
+  @JsonProperty("values")
+  private final List<Collection<V>> values;
+
+  private ImmutableMultimapProxy(
+      @JsonProperty("keys") final List<K> keys,
+      @JsonProperty("values") final List<Collection<V>> values) {
+    this.keys = checkNotNull(keys);
+    this.values = checkNotNull(values);
+    checkArgument(keys.size() == values.size());
+  }
+
+  public static <K, V> ImmutableMultimapProxy<K, V> forMultimap(Multimap<K, V> map) {
+    final List<K> keys = Lists.newArrayListWithCapacity(map.size());
+    final List<Collection<V>> values = Lists.newArrayListWithCapacity(map.size());
+    for (final Map.Entry<K, Collection<V>> e : map.asMap().entrySet()) {
+      keys.add(e.getKey());
+      values.add(e.getValue());
+    }
+    return new ImmutableMultimapProxy<>(keys, values);
+  }
+
+  public ImmutableMultimap<K, V> toImmutableMultimap() {
+    final ImmutableMap<K, Collection<V>> map = MapUtils.copyParallelListsToMap(keys, values);
+    final ImmutableMultimap.Builder<K, V> ret = ImmutableMultimap.builder();
+    for (final Map.Entry<K, Collection<V>> entry: map.entrySet()) {
+      ret.putAll(entry.getKey(), entry.getValue());
+    }
+    return ret.build();
+  }
+}
--- common-core-jackson-serialization-open/src/test/java/com/bbn/bue/common/serialization/jackson/TestSerialization.java
+++ common-core-jackson-serialization-open/src/test/java/com/bbn/bue/common/serialization/jackson/TestSerialization.java
@@ -1,20 +1,24 @@
 package com.bbn.bue.common.serialization.jackson;
 
 import com.bbn.bue.common.evaluation.FMeasureCounts;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableMap;
-
+import com.google.common.collect.ImmutableMultimap;
 import junit.framework.TestCase;
-
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class TestSerialization extends TestCase {
 
-  private final JacksonSerializer serializer = JacksonSerializer.forNormalJSON();
+  private final JacksonSerializer serializer = JacksonSerializer.builder().prettyOutput().build();
 
   @Test
   public void testFMeasureCounts() throws IOException {
@@ -34,4 +38,88 @@ public class TestSerialization extends TestCase {
     assertEquals(foo, serializer.deserializeFromString(serialized, foo.getClass()));
   }
 
+  @Test
+  public void testImmutableMapProxy() throws IOException {
+    final ImmutableMapWrapper expected = new ImmutableMapWrapper(ImmutableMap.of("a", 1));
+    final String serialized = serializer.writeValueAsString(expected);
+    assertEquals(expected, serializer.deserializeFromString(serialized, expected.getClass()));
+  }
+
+  @Test
+  public void testImmutableMultimapProxy() throws IOException {
+    final ImmutableMultimapWrapper expected = new ImmutableMultimapWrapper(
+        ImmutableMultimap.of("a", 1, "a", 2, "b", 3));
+    final String serialized = serializer.writeValueAsString(expected);
+    assertEquals(expected, serializer.deserializeFromString(serialized, expected.getClass()));
+  }
+
+  private static class ImmutableMapWrapper {
+    private final ImmutableMap map;
+
+    private ImmutableMapWrapper(ImmutableMap map) {
+      this.map = checkNotNull(map);
+    }
+
+    @JsonCreator
+    private static ImmutableMapWrapper fromJson(@JsonProperty("map") ImmutableMapProxy map) {
+      return new ImmutableMapWrapper(map.toImmutableMap());
+    }
+
+    @JsonProperty("map")
+    private ImmutableMapProxy mapProxy() {
+      return ImmutableMapProxy.forMap(map);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      ImmutableMapWrapper that = (ImmutableMapWrapper) o;
+      return Objects.equal(map, that.map);
+    }
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(map).toString();
+    }
+  }
+
+  private static class ImmutableMultimapWrapper {
+    private final ImmutableMultimap map;
+
+    private ImmutableMultimapWrapper(ImmutableMultimap map) {
+      this.map = checkNotNull(map);
+    }
+
+    @JsonCreator
+    private static ImmutableMultimapWrapper fromJson(@JsonProperty("map") ImmutableMultimapProxy map) {
+      return new ImmutableMultimapWrapper(map.toImmutableMultimap());
+    }
+
+    @JsonProperty("map")
+    private ImmutableMultimapProxy mapProxy() {
+      return ImmutableMultimapProxy.forMultimap(map);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      ImmutableMultimapWrapper that = (ImmutableMultimapWrapper) o;
+      return Objects.equal(map, that.map);
+    }
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(map).toString();
+    }
+  }
 }
