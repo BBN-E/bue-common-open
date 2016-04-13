37f58806fe56ee2ff6bafc4c3f091b5411937857
Ryan Gabbard <rgabbard@bbn.com>
2016-04-13 13:26:30 -0400

    Immutables style for package-private classes

---
+++ common-core-open/src/main/java/com/bbn/bue/common/TextGroupPackageImmutable.java
@@ -0,0 +1,39 @@
+package com.bbn.bue.common;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.annotations.Beta;
+
+import org.immutables.value.Value;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This is the text group's default style for immutable objects generated using the "Immutables.org"
+ * library. Use this whenever you use {@link org.immutables.value.Value.Immutable} for objects
+ * intended to be package-private unless you have a strong reason to do otherwise.
+ */
+@Beta
+@Target({ElementType.PACKAGE, ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS)
+@Value.Style(
+    // use the stripped abstract type name as the immutable type name
+    // rather than prefixing "Immutable" as the default would do.
+    // This is so that if we have _FlexibleEventMention,
+    // the generated implementation is FlexibleEventMention, not
+    // ImmutableFlexibleEventMention
+    typeImmutable = "*",
+    // the template class must be prefixed with _
+    typeAbstract = {"_*"},
+    // make the class annotated with this package private,
+    // so API users only see the generated implementation, not
+    // the template
+    visibility = Value.Style.ImplementationVisibility.PACKAGE)
+@JsonSerialize
+@JsonDeserialize
+public @interface TextGroupPackageImmutable {
+
+}
