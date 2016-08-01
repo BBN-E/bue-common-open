e600b1ddd827ad74a7cc0f1b3b19c58a03856123
Ryan Gabbard <rgabbard@bbn.com>
2016-08-01 19:27:12 -0400

    Workaround for ServiceLoader breaking in "fat JARs"
    ServiceLoader is used by Jackson to locate datatype modules, but it breaks when used with "fat JARs"
    (necessary for Hadoop, etc.) due to metadata file collisions.  This works around that by
    
    * always installing our standard modules
    * allowing you to explicitly bind additional modules to inject using Guice
    
    Closes #57

--- common-core-jackson-serialization-open/src/main/java/com/bbn/bue/common/serialization/jackson/BUECommonOpenModule.java
+++ common-core-jackson-serialization-open/src/main/java/com/bbn/bue/common/serialization/jackson/BUECommonOpenModule.java
@@ -10,10 +10,13 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import java.util.regex.Pattern;
 
+import javax.inject.Inject;
+
 public final class BUECommonOpenModule extends SimpleModule {
 
   private static final long serialVersionUID = 1L;
 
+  @Inject
   public BUECommonOpenModule() {
     super("BUECommonOpenModule", new Version(2, 1, 0, null, "com.bbn.bue", "common-core-open"));
   }
