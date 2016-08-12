7e81d7103e74e343afb295f267abd6be8e4503a8
Ryan Gabbard <rgabbard@bbn.com>
2016-08-12 14:47:42 -0400

    Temporal ERE fillers now load normalized times

--- nlp-core-open/src/main/java/com/bbn/nlp/corpora/ere/EREFiller.java
+++ nlp-core-open/src/main/java/com/bbn/nlp/corpora/ere/EREFiller.java
@@ -1,23 +1,35 @@
 package com.bbn.nlp.corpora.ere;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import com.google.common.base.Objects;
+import com.google.common.base.Optional;
+
+import javax.annotation.Nullable;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 
 public final class EREFiller implements ERESpanning {
   private final String id;
   private final String type;
   private final ERESpan extent;
+  @Nullable
+  private final String nomTime;
 
-  private EREFiller(final String id, final String type, final ERESpan extent) {
+  private EREFiller(final String id, final String type, final ERESpan extent, @Nullable final String nomTime) {
     this.id = checkNotNull(id);
     this.type = checkNotNull(type);
     this.extent = checkNotNull(extent);
+    // nullable
+    this.nomTime = nomTime;
   }
-  
+
   public static EREFiller from(final String id, final String type, final ERESpan extent) {
-    return new EREFiller(id, type, extent);
+    return new EREFiller(id, type, extent, null);
+  }
+
+  public static EREFiller fromTime(final String id, final String type, String normalizedTime,
+      final ERESpan extent) {
+    return new EREFiller(id, type, extent, normalizedTime);
   }
 
   @Override
@@ -28,12 +40,16 @@ public final class EREFiller implements ERESpanning {
   public String getType() {
     return type;
   }
-  
+
   @Override
   public ERESpan getExtent() {
     return extent;
   }
- 
+
+  public Optional<String> getNormalizedTime() {
+    return Optional.fromNullable(nomTime);
+  }
+
   @Override
   public int hashCode() {
     return Objects.hashCode(id);
@@ -53,7 +69,7 @@ public final class EREFiller implements ERESpanning {
     final EREFiller other = (EREFiller) obj;
     return Objects.equal(id, other.id);
   }
-  
+
 }
 
 
