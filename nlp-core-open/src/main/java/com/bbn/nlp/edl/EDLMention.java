c2a70f9775be5b504d2d58d1200908c624768758
Ryan Gabbard <rgabbard@bbn.com>
2016-09-28 11:40:03 -0400

    Immutables-ize EDLMention

--- nlp-core-open/src/main/java/com/bbn/nlp/edl/EDLMention.java
+++ nlp-core-open/src/main/java/com/bbn/nlp/edl/EDLMention.java
@@ -1,125 +1,63 @@
 package com.bbn.nlp.edl;
 
+import com.bbn.bue.common.TextGroupImmutable;
 import com.bbn.bue.common.evaluation.ScoringTypedOffsetRange;
 import com.bbn.bue.common.strings.offsets.CharOffset;
 import com.bbn.bue.common.strings.offsets.OffsetRange;
 import com.bbn.bue.common.symbols.Symbol;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.common.base.Function;
-import com.google.common.base.Objects;
-import com.google.common.collect.Range;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
+import org.immutables.func.Functional;
+import org.immutables.value.Value;
 
 /**
  * Represents a mention for the Entity Detection and Linking scorer to the degree
  * necessary for the Lorelei NER evaluations.  Does not yet address KB linking.
  */
-public final class EDLMention {
+@TextGroupImmutable
+@Value.Immutable
+@Functional
+@JsonSerialize
+@JsonDeserialize
+public abstract class EDLMention {
+  public abstract Symbol runId();
+  public abstract String mentionId();
+  public abstract Symbol documentID();
+  public abstract String headString();
+  public abstract OffsetRange<CharOffset> headOffsets();
+  public abstract Symbol mentionType();
+  public abstract Symbol entityType();
+  public abstract double confidence();
 
-  private final Symbol runId;
-  private final String mentionId;
-  private final Symbol documentID;
-  private final String headString;
-  private final OffsetRange<CharOffset> headOffsets;
-  private final Symbol mentionType;
-  private final Symbol entityType;
-  private final double confidence;
-
-  private EDLMention(final Symbol runId, final String mentionId,
-      final Symbol documentID, final String headString,
-      final OffsetRange<CharOffset> headOffsets,
-      final Symbol mentionType, final Symbol entityType, final double confidence) {
-    this.runId = checkNotNull(runId);
-    this.mentionId = checkNotNull(mentionId);
-    this.documentID = checkNotNull(documentID);
-    this.headString = checkNotNull(headString);
-    this.headOffsets = checkNotNull(headOffsets);
-    this.mentionType = checkNotNull(mentionType);
-    this.entityType = checkNotNull(entityType);
-    this.confidence = confidence;
-    checkArgument(Range.openClosed(0.0, 1.0).contains(confidence),
-        "Confidence must be in (0,1.0] but got %s", confidence);
-  }
-
+  /**
+   * @deprecated Prefer {@link Builder}.
+   */
+  @Deprecated
   public static EDLMention create(final Symbol runId, final String mentionId,
       final Symbol documentID, final String headString,
       final OffsetRange<CharOffset> headOffsets,
       final Symbol mentionType, final Symbol entityType, final double confidence)
   {
-    return new EDLMention(runId, mentionId, documentID, headString, headOffsets,
-        mentionType, entityType, confidence);
+    return new Builder().runId(runId).mentionId(mentionId).documentID(documentID)
+        .headString(headString).headOffsets(headOffsets)
+        .mentionType(mentionType).entityType(entityType).confidence(confidence).build();
   }
 
-  public Symbol runId() {
-    return runId;
-  }
-
-  public String mentionId() {
-    return mentionId;
-  }
-
-  public Symbol documentID() {
-    return documentID;
-  }
-
-  public String headString() {
-    return headString;
-  }
-
-  public OffsetRange<CharOffset> headOffsets() {
-    return headOffsets;
-  }
-
-  public Symbol mentionType() {
-    return mentionType;
-  }
-
-  public Symbol entityType() {
-    return entityType;
-  }
-
-  public double confidence() {
-    return confidence;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects
-        .hashCode(runId, mentionId, documentID, headString, headOffsets, mentionType, entityType,
-            confidence);
-  }
-
-  @Override
-  public boolean equals(final Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null || getClass() != obj.getClass()) {
-      return false;
-    }
-    final EDLMention other = (EDLMention) obj;
-    return Objects.equal(this.runId, other.runId)
-        && Objects.equal(this.mentionId, other.mentionId)
-        && Objects.equal(this.documentID, other.documentID)
-        && Objects.equal(this.headString, other.headString)
-        && Objects.equal(this.headOffsets, other.headOffsets)
-        && Objects.equal(this.mentionType, other.mentionType)
-        && Objects.equal(this.entityType, other.entityType)
-        && Objects.equal(this.confidence, other.confidence);
-  }
+  public static class Builder extends ImmutableEDLMention.Builder {}
 
   /**
-   * Guava {@link Function} mapping from {@code EDLMention}s to their entity types.
+   * @deprecated Prefer {@link EDLMentionFunctions#entityType()}
    */
+  @Deprecated
   public static Function<EDLMention, Symbol> entityTypeFunction() {
     return EntityTypeFunction.INSTANCE;
   }
 
   /**
-   * Guava {@link Function} mapping from {@code EDLMention}s to their
-   * mention types.
+   * @deprecated Prefer {@link EDLMentionFunctions#mentionType()}
    */
   public static Function<EDLMention, Symbol> mentionTypeFunction() {
     return MentionTypeFunction.INSTANCE;
@@ -134,6 +72,10 @@ public final class EDLMention {
     return AsTypedOffsetRangeFunction.INSTANCE;
   }
 
+  /**
+   * @deprecated Prefer {@link EDLMentionFunctions#entityType()}
+   */
+  @Deprecated
   private enum EntityTypeFunction implements Function<EDLMention, Symbol> {
     INSTANCE {
       @Override
@@ -143,6 +85,10 @@ public final class EDLMention {
     }
   }
 
+  /**
+   * @deprecated Prefer {@link EDLMentionFunctions#mentionType()}
+   */
+  @Deprecated
   private enum MentionTypeFunction implements Function<EDLMention, Symbol> {
     INSTANCE {
       @Override
