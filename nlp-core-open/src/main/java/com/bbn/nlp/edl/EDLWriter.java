6a547e530f13b2e6cc76851f355aa2bb3cecc2ed
Constantine Lignos <clignos@bbn.com>
2017-07-19 21:15:07 -0400

    Support KB ids in EDLMention
    Changes the EDL classes to support an optional KB identifier, with a
    helper for creating NIL KB IDs. Code depending on EDLWriter.create()
    will continue to see the old behavior of writing `NIL` for the KB ID
    of all EDL mentions, but newer code that uses EDLWriter.Builder will
    be able to specify a default KB ID to be used for any EDL mentions
    with absent KB IDs.

--- nlp-core-open/src/main/java/com/bbn/nlp/edl/EDLWriter.java
+++ nlp-core-open/src/main/java/com/bbn/nlp/edl/EDLWriter.java
@@ -1,24 +1,43 @@
 package com.bbn.nlp.edl;
 
+import com.bbn.bue.common.TextGroupImmutable;
+
+import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
 import com.google.common.io.CharSink;
+
+import org.immutables.value.Value;
 
 import java.io.IOException;
 import java.util.List;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 /**
- * Writes files in the submission format for the TAC KBP Entity Detection and Linking eval. This is
- * focused on Lorelei NER, so it ignores the KB IDs.
+ * Writes files in the submission format for the TAC KBP Entity Detection and Linking eval.
  */
-public final class EDLWriter {
+@TextGroupImmutable
+@Value.Immutable
+public abstract class EDLWriter {
 
-  private EDLWriter() {
-  }
+  /**
+   * A default KB ID to assign to {@link EDLMention}s with absent KB IDs.
+   */
+  public abstract Optional<String> defaultKbId();
 
+  /**
+   * @deprecated Prefer {@link Builder}. To preserve legacy behavior, the constructed object
+   * uses a default KB ID of {@code NIL}.
+   */
+  @Deprecated
   public static EDLWriter create() {
-    return new EDLWriter();
+    return new EDLWriter.Builder().defaultKbId("NIL").build();
   }
 
+  /**
+   * Writes out the specified {@link EDLMention}s. If a {@link #defaultKbId} was not specified,
+   * an exception will be thrown if any EDL mentions have an absent KB ID.
+   */
   public void writeEDLMentions(Iterable<EDLMention> edlMentions, CharSink sink) throws IOException {
     final List<String> lines = Lists.newArrayList();
 
@@ -29,10 +48,15 @@ public final class EDLWriter {
     sink.writeLines(lines, "\n");
   }
 
+  public static class Builder extends ImmutableEDLWriter.Builder {}
+
   private String toLine(final EDLMention edlMention) {
+    final Optional<String> kbId = edlMention.kbId().or(defaultKbId());
+    checkArgument(kbId.isPresent(),
+        "EDL mention %s does not have a KB ID and no default was specified", kbId);
     return edlMention.runId() + "\t" + edlMention.mentionId() + "\t" + edlMention.headString()
         + "\t" + edlMention.documentID() + ":" + edlMention.headOffsets().startInclusive().asInt()
-        + "-" + edlMention.headOffsets().endInclusive().asInt() + "\t" + "NIL\t"
+        + "-" + edlMention.headOffsets().endInclusive().asInt() + "\t" + kbId.get() + "\t"
         + edlMention.entityType() + "\t" + edlMention.mentionType() + "\t"
         + edlMention.confidence();
   }
--- nlp-core-open/src/test/java/com/bbn/nlp/edl/EDLTest.java
+++ nlp-core-open/src/test/java/com/bbn/nlp/edl/EDLTest.java
@@ -2,8 +2,10 @@ package com.bbn.nlp.edl;
 
 import com.bbn.bue.common.io.StringSink;
 import com.bbn.bue.common.strings.offsets.OffsetRange;
+import com.bbn.bue.common.symbols.Symbol;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.CharSource;
 import com.google.common.io.Resources;
@@ -13,6 +15,7 @@ import org.junit.Test;
 import java.io.IOException;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
 
 public class EDLTest {
 
@@ -27,9 +30,12 @@ public class EDLTest {
     assertEquals("BBN1", secondMention.runId().asString());
     assertEquals("ment-2", secondMention.mentionId());
     assertEquals("BBN Technologies", secondMention.headString());
+    assertEquals("12345678", secondMention.kbId().get());
     assertEquals("NYT20081231002", secondMention.documentID().asString());
     assertEquals(OffsetRange.charOffsetRange(0, 15), secondMention.headOffsets());
     assertEquals(0.765, secondMention.confidence(), .0001);
+    final EDLMention firstMention = edlMentions.get(0);
+    assertEquals("NIL", firstMention.kbId().get());
   }
 
   @Test
@@ -46,5 +52,32 @@ public class EDLTest {
     final ImmutableList<EDLMention> reloadedEDLMentions =
         edlLoader.loadEDLMentionsFrom(CharSource.wrap(tmp.getString()));
     assertEquals(edlMentions, reloadedEDLMentions);
+  }
+
+  @Test
+  public void edlKbIdTest() {
+    final EDLMention ment = new EDLMention.Builder()
+        .confidence(1.0)
+        .entityType(Symbol.from("ORG"))
+        .mentionType(Symbol.from("NAM"))
+        .documentID(Symbol.from("NW_1234"))
+        .headOffsets(OffsetRange.charOffsetRange(0, 2))
+        .runId(Symbol.from("test"))
+        .mentionId("ment-1")
+        .headString("BBN")
+        .build();
+
+    assertEquals(Optional.absent(), ment.kbId());
+    assertEquals(Optional.of("123"),
+        new EDLMention.Builder().from(ment).kbId("123").build().kbId());
+    assertEquals(Optional.of("NIL123"),
+        new EDLMention.Builder().from(ment).nilKbId("123").build().kbId());
+    // Verify an empty ID throws an exception
+    try {
+      new EDLMention.Builder().from(ment).kbId("").build();
+      fail();
+    } catch (IllegalArgumentException e) {
+      // Pass
+    }
   }
 }
